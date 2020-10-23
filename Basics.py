7db46072b9efd504bfc226a231a54b9cfde4d882
atominize <atominize@gmail.com>
2020-10-23 12:38:30 +0000

    First commit

---
+++ Basics.py
@@ -0,0 +1,32 @@
+# 1. doesn't need ; to show the end of statement
+# 2. Indentation
+
+# number = 1
+#     lastName = "Robert"
+
+print("Greetings World")
+
+
+#       Variables in Python
+# Python has no command for declaring a variable.
+
+x = 5
+y = "How are you?"
+
+#       Comments in Python
+# This is a comment
+
+# Uses of comments
+# 1. Comments can be used to explain Python code.
+# 2. Comments can be used to make the code more readable.
+# 3. Comments can be used to prevent execution when testing code.
+
+print(y) # This is a comment
+
+sentence = """
+This is a comment
+written in 
+more than just one line
+"""
+
+print(sentence)
---
+++ main.py
@@ -0,0 +1,10 @@
+print("Hello World")
+
+firstNumber = 2
+secondNumber = 4
+result = firstNumber + secondNumber
+print(result)
+
+# Data Types
+# 1. numbers
+# 2. strings
